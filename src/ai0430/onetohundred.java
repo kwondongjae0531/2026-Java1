433fdc5dbcce8a0ca51f33c61a4ecedd8c5cf054
kwondongjae0531 <charlotte0531@naver.com>
2026-04-30 11:44:11 +0900

    commit1111wdssfdssfs

---
+++ src/ai0430/onetohundred.java
@@ -0,0 +1,13 @@
+package ai0430;
+
+public class onetohundred {
+    public static void main(String[] args) {
+    for (int i = 1; i <= 100; i++){
+        System.out.print(i + " ");
+
+        if(i % 10 == 0){
+            System.out.println();
+        }
+    }
+    }
+}
