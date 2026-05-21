5c0eded58ae73385d8c3ea0ac810b2c8f100abe0
kwondongjae0531 <charlotte0531@naver.com>
2026-05-21 11:45:32 +0900

    hi

---
+++ src/ai0521/rabbit/Rabbit.java
@@ -0,0 +1,41 @@
+package ai0521.rabbit;
+
+public class Rabbit {
+
+    private String shape;
+    private int xPos;
+    private int yPos;
+
+    public Rabbit() {
+    }
+
+    public Rabbit(String shape) {
+        this.shape = shape;
+    }
+
+    public String getShape() {
+        return shape;
+    }
+
+    public void setShape(String shape) {
+        this.shape = shape;
+    }
+
+    public int getxPos() {
+        return xPos;
+    }
+
+    public void setxPos(int xPos) {
+        this.xPos = xPos;
+    }
+
+    public int getyPos() {
+        return yPos;
+    }
+
+    public void setyPos(int yPos) {
+        this.yPos = yPos;
+    }
+}
+
+
