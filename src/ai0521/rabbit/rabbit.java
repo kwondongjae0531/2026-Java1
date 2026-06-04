916c93ebfb3817ba8be88bd18a2ed4812222b5e8
kwondongjae0531 <charlotte0531@naver.com>
2026-06-04 18:54:41 +0900

    Java 7장 수정 및 8장 상속의 개념, 사용방법, 리소스 사용제한, 오버라이딩

---
+++ src/ai0521/rabbit/rabbit.java
@@ -0,0 +1,67 @@
+package ai0521.rabbit;
+
+public class rabbit {
+
+    private String shape; // 토끼의 모양
+    private int xPos; // X 위치
+    private int yPos; // Y 위치
+
+
+    void setPosition(int x, int y) {
+        xPos = x;
+        yPos = y;
+
+    }
+    public rabbit() {
+        this.shape = shape;
+    }
+
+    public rabbit(String shape) {
+        this.shape = shape;
+    }
+
+    public String getShape() {
+        return shape;
+    }
+
+    public void setShape(String shape) {
+        this.shape = shape;
+    }
+
+    public int getxPos() {
+        return xPos;
+    }
+
+
+    public void setxPos(int xPos) {
+        this.xPos = xPos;
+    }
+
+    public int getyPos() {
+        return yPos;
+    }
+
+    public void setyPos(int yPos) {
+        this.yPos = yPos;
+    }
+
+    public void setLocation(int xPos, int yPos){
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
+    // 토끼의 모양, 위치 출력 메소드
+    public void printInfo(){
+        System.out.printf("%s 모양의 토끼는 (%d, %d) 좌표에 위치하고 있다.\n", shape, xPos, yPos);
+    }
+
+    //    x축으로 10씩 오른쪽으로 이동하는 메소드
+    public void moverigthTen(){
+        xPos += 10;
+    }
+    //    x축으로 10씩 왼쪽으로 이동하는 메소드
+    public void moveleftTen() {
+        xPos -= 10;
+    }
+}
+
+
