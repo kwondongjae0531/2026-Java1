4f1bfb1adc8333703885766820526c3058a546ba
kwondongjae0531 <charlotte0531@naver.com>
2026-09-22 14:11:38 +0900

    11장 파일 입출력 - 라인번호 출력 LAB

--- src/ai0922/Lab_AddLineNumber.java
+++ src/ai0922/Lab_AddLineNumber.java
@@ -1,40 +1,32 @@
-package ai0915;
+package ai0922;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class FileReaderTest3 {
-    // 파일의 내용 전체를 거꾸로 출력
+public class Lab_AddLineNumber {
     public static void main(String[] args) {
         try {
-            StringBuilder sb = new StringBuilder();
             // File을 읽어오기 위한 입력스트림(inputStream) 생성
 
             BufferedReader br = new BufferedReader(
-                    new FileReader("D:\\FileIOTest\\myData1.txt")
-            );
+                    new FileReader("D:\\FileIOTest\\myData1.txt"));
 
             String line = "";
+            int cnt = 1;
 
 
-// File에서 한 줄씩 읽어오기
-
+// File의 끝까지 File에서 한 줄씩 읽어오기
             while (true) {
                 line = br.readLine();
                 if (line == null)
                     break;
-                sb.append(line).append("\n");
+                System.out.println(cnt++ +" : " +line);
             }
 
 // FileInputStream 닫기
             br.close();
-
-
-            sb.reverse();
-
-            System.out.println(sb);
 
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
@@ -44,3 +36,4 @@ public class FileReaderTest3 {
         }
     }
 }
+
